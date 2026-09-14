7414a4fdd21aa1044b00ba635cef0a62e495cc70
md samiullah <mdsamiullah44444@gmail.com>
2026-09-14 17:37:02 +0530

    Add: lowest-common-ancestor-of-a-binary-tree-iii - submission-0

---
+++ Data Structures & Algorithms/lowest-common-ancestor-of-a-binary-tree-iii/submission-0.java
@@ -0,0 +1,25 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node parent;
+};
+*/
+
+class Solution {
+    public Node lowestCommonAncestor(Node p, Node q) {
+
+        Node p1 = p;
+        Node q1 = q;
+
+        while (p1 != q1) {
+
+            p1 = (p1 == null) ? q : p1.parent;
+            q1 = (q1 == null) ? p : q1.parent;
+        }
+
+        return p1;
+    }
+}
